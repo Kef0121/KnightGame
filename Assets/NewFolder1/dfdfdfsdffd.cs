0ecfb52dace21f91bfe2821b3bdc553034f67c57
Kef0121 <kef012@proton.me>
2026-08-07 19:48:57 -0300

    Add source code

---
+++ Assets/NewFolder1/dfdfdfsdffd.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float moveForce = 3f;
+    public float jumpForce = 10f;
+
+    private bool onGround;
+
+    private int maxJumps = 1;
+    private int jumps;
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void fixedUpdate()
+    {
+        //
+        float horizontal = Input.GetAxis("Horizontal");
+        Movement(horizontal);
+        Jump();
+
+    }
+    //Jump mechanic
+    void Jump()
+    {
+
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+    }
+    //
+    void Movement(float horizontal)
+    {
+
+        rb2d.velocity = new Vector2(horizontal, rb2d.velocity.y);
+
+    }
+}
+
---
+++ Assets/Scripts/knight_main.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class knight_main : MonoBehaviour
+{
+
+    //Jump gizmos
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.1f;
+    private bool onGround;
+    private int maxJumps = 1;
+    private int Jumps;
+
+    //RigidBody2d
+    private Rigidbody2D rb;
+
+    //facing left/right thingies
+     
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Check if grounded
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (onGround)
+        {
+            Jumps = 0;
+        }
+
+
+        // Move left/right
+        float move = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+
+        // Jump
+        if (Jumps < maxJumps && Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            Jumps++;
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+        }
+    }
+}
+
